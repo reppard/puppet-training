2791c1d673968c9316428c2101d3b32b793b55d4
reppard <reppard@puppetlabs.vm>
2014-10-29 14:43:32 +0000

    Fixed hostname alias

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -1,6 +1,7 @@
 class hosts { 
-  host { 'reppard.puppetlabs.vm reppard':
-    ensure => present,
-    ip     => '127.0.0.1',
+  host { 'reppard.puppetlabs.vm':
+    ensure       => present,
+    ip           => '127.0.0.1',
+    host_aliases => ['reppard'],
   }
 }
