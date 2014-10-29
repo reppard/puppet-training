cb36a74a83acd3e69e2167876d108e96f0fe4010
reppard <reppard@puppetlabs.vm>
2014-10-29 14:10:15 +0000

    Adding tmux config

--- modules/tmux/manifests/init.pp
+++ modules/tmux/manifests/init.pp
@@ -1,4 +1,9 @@
 class tmux {
+  file { '/root/.tmux.conf':
+    ensure =>  present,
+    source => 'https://raw.githubusercontent.com/reppard/dotfiles/master/tmux.conf',
+  }
+
   package { 'tmux':
     ensure => installed,
   }
