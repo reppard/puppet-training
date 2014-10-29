2c16cc61da375bf4f065e4a82f92f3be6a85f948
reppard <reppard@puppetlabs.vm>
2014-10-29 15:03:43 +0000

    Tmux.conf

--- modules/tmux/manifests/init.pp
+++ modules/tmux/manifests/init.pp
@@ -1,12 +1,14 @@
 class tmux{
 
-  #  exec { 'get source':
-  #  command => "wget https://raw.githubusercontent.com/reppard/dotfiles/master/tmux.conf /tmp/tmux.conf"
-  #}
-
-  #file { '/root/.tmux.conf':
-  #  ensure =>  present,
-  #}
+  exec { 'get source':
+    command => "/usr/bin/wget -O /tmp/tmux.conf https://raw.githubusercontent.com/reppard/dotfiles/master/tmux.conf"
+  }
+  ->
+  file { '.tmux.conf':
+    path   => '/root/.tmux.conf',
+    source => '/tmp/tmux.conf',
+    ensure =>  present,
+  }
 
   package { 'tmux':
     ensure => installed,
